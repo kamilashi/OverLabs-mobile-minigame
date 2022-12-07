ccc8906508bb938bde6482993fa8ab5d80e851b0
Kamila <64157493+kamilashi@users.noreply.github.com>
2022-12-07 19:10:49 +0100

    initial commit

---
+++ Assets/Scripts/FloorManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    private float _tileSize = 1;
+    private float _gridWidth = 6;
+    private float _gridHeight = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateTerrain();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateTerrain()
+    {
+        GameObject refTile = (GameObject)Instantiate(Resources.Load("TileDefault"));
+        for (int i = 0; i< _gridWidth; i++ )
+        {
+            for (int j = 0; j < _gridHeight; j++)
+            {
+                Vector2 position = new Vector2(i - (_gridWidth-_tileSize)/2, j - (_gridHeight - _tileSize) / 2) * _tileSize;
+                GameObject tile = (GameObject) Instantiate(refTile, position, Quaternion.identity, transform);
+            }
+
+        }
+        Destroy(refTile);
+
+    }
+}
